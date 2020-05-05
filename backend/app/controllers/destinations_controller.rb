d380a06269473dbbf86510a92295e7642de218a9
github username <github email address>
2020-05-05 16:55:31 -0400

    updated index in destinations controller

--- backend/app/controllers/destinations_controller.rb
+++ backend/app/controllers/destinations_controller.rb
@@ -1,7 +1,49 @@
 class DestinationsController < ActionController::API
     def index
-        destinations = Destination.all
-        render json: destinations, except: [:created_at, :updated_at]
+        continents = {
+            "North America": {
+                "countries":{
+                    "America": Destination.select{|d| d.country == "America"},
+                    "Canada": Destination.select{|d| d.country == "Canada"},
+                    "Mexico": Destination.select{|d| d.country == "Mexico"},
+                }
+            } ,
+            "Europe": {
+                "countries":{
+                    "England": Destination.select{|d| d.country == "England"},
+                    "France": Destination.select{|d| d.country == "Canada"},
+                    "Italy": Destination.select{|d| d.country == "Italy"}
+                }
+            },
+            "Asia": {
+                "countries":{
+                    "Japan": Destination.select{|d| d.country == "Japan"},
+                    "China": Destination.select{|d| d.country == "China"},
+                    "India": Destination.select{|d| d.country == "India"}
+                }   
+            },
+            "South America": {
+                "countries":{
+                    "Brazil": Destination.select{|d| d.country == "Brazil"},
+                    "Peru": Destination.select{|d| d.country == "Peru"},
+                    "Argentina": Destination.select{|d| d.country == "Argentina"}
+                }
+            },
+            "Africa": {
+                "countries":{
+                    "Tanzania": Destination.select{|d| d.country == "Tanzania"},
+                    "Egypt": Destination.select{|d| d.country == "Egypt"},
+                    "South Africa": Destination.select{|d| d.country == "South Africa"}
+                }
+            },
+            "Oceania": {
+                "countries":{
+                    "Austrailia": Destination.select{|d| d.country == "Austrailia"},
+                    "New Zealand": Destination.select{|d| d.country == "New Zealand"}
+                }   
+            }
+        }
+        render json: continents, except: [:created_at, :updated_at]
     end
 
     def show
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,10 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
 Destination.destroy_all
+Postcard.destroy_all
 
 u1 = User.create(name: "Brian")
 u2 = User.create(name: "Jared")
 u2 = User.create(name: "Dutch")
 
-d1 = Destination.create(name: "Tahiti", description: "We just need some muneh", region: "North America", country: "Merica", category: "Middle Of Nowhere")
-d2 = Destination.create(name: "New York", description: "It looks better than it smells", region: "North America", country: "USA", category: "Mega City")
+d1 = Destination.create(name: "Tahiti", description: "We just need some muneh", region: "North America", country: "America", category: "Middle Of Nowhere")
+d2 = Destination.create(name: "New York", description: "It looks better than it smells", region: "North America", country: "America", category: "Mega City")
+d3 = Destination.create(name: "Sydney", description: "Come toss a shrimp on the barbey, mate!", region: "Austrailia", country: "Austrailia", category: "City")
+d4 = Destination.create(name: "Bejing", description: "Now offering bike rides on the Great Wall", region: "Asia", country: "China", category: "Mega City")
+d5 = Destination.create(name:"Tanzania", description: "Egyptian tans are second to none!", region: "Africa", country: "South Africa", category: "Resort")
+d6 = Destination.create(name: "London", description: "Fish, Chips and Tea!", region: "Europe", country: "England", category: "City")
+
